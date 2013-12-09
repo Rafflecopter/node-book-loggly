36797d0db94532c1b52db93bcdede3daa7a8573d
Jon Eisen <jon@metricfeat.com>
2013-12-09 15:01:36 -0700

    Add book-loggly

--- README.md
+++ README.md
@@ -1,4 +1,67 @@
-node-book-loggly
-================
+# book-loggly
 
-node-book middleware for loggly
+[loggly](http://loggly.com) middleware for the [book](https://github.com/defunctzombie/node-book) logging framework.
+
+## Install
+
+```
+npm install book book-loggly --save
+```
+
+## Use
+
+```javascript
+var log = require('book').default();
+
+var logglyConfig = {
+  "token": "your-really-long-token-you-got-when-you-created-an-http-input",
+  "subdomain": "your-subdomain",
+  "auth": {
+    "username": "your-username",
+    "password": "your-password"
+  },
+  // A custom book-loggly option to ignore logs at ignore_levels and above
+  // 0: panic, 1: error, 2: warning, 3: info, 4: debug (default), 5: trace
+  "ignore_levels": log.TRACE
+};
+
+log.use(require('book-loggly')(logglyConfig));
+
+
+// Now just log as usual
+log.warn("Hey there!")
+log.info("Logging this object", {some: 'object'})
+log.error(new Error("test error!"))
+```
+
+## Internals
+
+`book-loggly` uses the loggly JSON capability to log objects. The general form of these objects is:
+
+```json
+{
+  "isError": false,
+  "message": "message",
+  "level_code": 3,
+  "level": "warning",
+  "extra": {
+    "extra_parameters": "that were added by other middleware",
+    "object fields": "from objects passed into the log function"
+  }
+}
+```
+
+## uncaughtException
+
+If there is an error sending the packet to loggly, an error will be thrown. To catch it, use `uncaughtException`. You can do anything here, but perhaps you want to exit.
+
+```javascript
+process.on('uncaughtException', function(err) {
+    log.panic(err);
+    setTimeout(process.exit.bind(process, 1), 1500);
+})
+```
+
+## License
+
+See LICENSE file.
---
+++ index.js
@@ -0,0 +1,86 @@
+// node-book-loggly/index.js
+// the loggly middleware for the node book logging framework
+
+// builtin
+var http = require('http');
+
+// vendor
+var extend = require('xtend')
+var loggly = require('loggly')
+
+// level is a numeric value for book from [0, 5]
+// panic, error, warning, info, debug, trace
+var levels = ['fatal', 'error', 'warning', 'info', 'debug', 'trace'];
+
+module.exports = function(config) {
+    var ignore_levels = config.ignore_levels || 4;
+
+    config.json = true
+    var client = loggly.createClient(config)
+
+    return function() {
+        var self = this;
+
+        // default is error
+        var lvl = 'error';
+
+        if (self.level < levels.length) {
+            lvl = levels[self.level];
+        }
+
+        // ignore anything below warning
+        if (self.level > ignore_levels) {
+            return;
+        }
+
+        var extra = extend({}, self);
+        delete extra.level;
+
+        // add our fields to the message
+        var packet = {
+            isError: false,
+            message: self.message,
+            extra: extra,
+            level_code: self.level,
+            level: lvl
+        };
+
+        for (var idx=0 ; idx < arguments.length ; ++idx) {
+            var arg = arguments[idx];
+
+            // http interface handling
+            if (arg instanceof http.IncomingMessage) {
+                packet[interfaces.http.key] = interfaces.http(arg);
+            }
+            // error will be handled below
+            // only allowed as first argument
+            else if (arg instanceof Error) {
+                continue;
+            }
+            // if user passed an object, then capture extra fields
+            else if (arg instanceof Object) {
+                Object.keys(arg).forEach(function(key) {
+                    extra[key] = arg[key];
+                });
+            }
+        }
+
+        // if the first argument is an error, capture it as the error interface
+        if (arguments[0] instanceof Error) {
+            var err = arguments[0];
+
+            if (Object.keys(err).length > 0) {
+                extra.error = err;
+            }
+
+            packet.isError = true;
+        }
+
+        client.log(packet, function (err) {
+            // Use uncaughtException to catch these errors
+            throw err
+        });
+    }
+}
+
+var noop = function() {};
